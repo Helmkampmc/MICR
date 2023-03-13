799d377308e0f85741a85c37821e252d6bf0cb9a
Helmkampmc <120995077+Helmkampmc@users.noreply.github.com>
2023-03-13 15:50:30 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -1,55 +1,27 @@
-import pandas as pd
 import streamlit as st
-import altair as alt
+import pandas as pd
 
-# Read in the crime data
-url = 'https://www.michigan.gov/msp/-/media/Project/Websites/msp/micr-assets/2021/Agency-Crime-Stats_2021.xlsx'
-df = pd.read_excel(url)
+# Load data from GitHub
 
-# Rename columns for easier access
-df.columns = [col.replace(" ", "_") for col in df.columns]
+df = pd.read_csv("Michigan_crime_2021.csv")
 
-# Create a sidebar with filtering options
-st.sidebar.title('Filter Options')
-filter_col = st.sidebar.selectbox('Select Column to Filter By', options=df.columns)
+# Create dropdown for selecting ORI - Agency
+ori_list = df['ORI - Agency'].unique()
+ori_selection = st.selectbox('Select an ORI - Agency:', ori_list)
 
-# Create a filter for the selected column
-filter_val = st.sidebar.text_input(f'Select Value for {filter_col}', '')
+# Filter data based on ORI - Agency selection
+filtered_df = df[df['ORI - Agency'] == ori_selection]
 
-# Filter the data by the selected column and value
-if filter_val != '':
-    filtered_df = df[df[filter_col].str.contains(filter_val, case=False)]
-else:
-    filtered_df = df
+# Display filtered data in Streamlit
+st.write(f'### Michigan Crime Data (2021) - {ori_selection}')
+st.write(filtered_df)
 
-# Create a title for the app
-st.title('Michigan Crime Data')
+# Show summary statistics for filtered data
+st.write(f'### Summary Statistics - {ori_selection}')
+st.write(filtered_df.describe())
 
-# Create a bar chart showing the number of incidents by agency type
-st.title('Number of Incidents by Agency Type')
-chart1 = alt.Chart(filtered_df).mark_bar().encode(
-    x=alt.X('Agency_Type:N', sort=alt.EncodingSortField(field='Total_Incidents', order='descending')),
-    y=alt.Y('Total_Incidents:Q', title='Number of Incidents'),
-    tooltip=['Agency_Type', 'Total_Incidents']
-).properties(
-    width=700,
-    height=500
-)
-st.altair_chart(chart1)
+# Show bar chart of crime types for filtered data
+st.write(f'### Crimes by Type - {ori_selection}')
+crime_counts = filtered_df['Offense Category'].value_counts()
+st.bar_chart(crime_counts)
 
-# Create a bar chart showing the number of arrests by agency type
-st.title('Number of Arrests by Agency Type')
-chart2 = alt.Chart(filtered_df).mark_bar().encode(
-    x=alt.X('Agency_Type:N', sort=alt.EncodingSortField(field='Total_Arrests', order='descending')),
-    y=alt.Y('Total_Arrests:Q', title='Number of Arrests'),
-    tooltip=['Agency_Type', 'Total_Arrests']
-).properties(
-    width=700,
-    height=500
-)
-st.altair_chart(chart2)
-
-# Create a table showing the total incidents and arrests for each agency type
-st.title('Total Incidents and Arrests by Agency Type')
-sum_df = filtered_df.groupby('Agency_Type')[['Total_Incidents', 'Total_Arrests']].sum()
-st.table(sum_df)
