e3e5c52847c51ab934524b9bc6e1c573a59b7b0e
Helmkampmc <120995077+Helmkampmc@users.noreply.github.com>
2023-03-13 15:01:11 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -11,14 +11,13 @@ df.columns = [f'{col[0]}_{col[1].replace(" ", "_")}' for col in df.columns]
 
 # Create a sidebar with filtering options
 st.sidebar.title('Filter Options')
-year = st.sidebar.slider('Select Year', min_value=1975, max_value=2020, step=1, value=2020)
 offense = st.sidebar.selectbox('Select Offense', options=df.columns[1:])
 
-# Filter the data by year and offense
-filtered_df = df.loc[df[f'Year_Ending_{year}'] == year, ['Offense_Category', offense]].dropna()
+# Filter the data by offense
+filtered_df = df[['Offense_Category', offense]].dropna()
 
 # Create a bar chart showing the number of offenses by offense category
-st.title(f'{offense} Offenses in {year}')
+st.title(f'{offense} Offenses')
 chart = alt.Chart(filtered_df).mark_bar().encode(
     x=alt.X('Offense_Category:N', sort=alt.EncodingSortField(field=offense, order='descending')),
     y=alt.Y(offense, title='Number of Offenses'),
@@ -29,7 +28,7 @@ chart = alt.Chart(filtered_df).mark_bar().encode(
 )
 st.altair_chart(chart)
 
-# Create a table showing the top 10 counties for the selected offense and year
-st.title(f'Top 10 Counties for {offense} Offenses in {year}')
-county_df = df.loc[df[f'Year_Ending_{year}'] == year, ['County', offense]].dropna().sort_values(offense, ascending=False).head(10)
+# Create a table showing the top 10 counties for the selected offense
+st.title(f'Top 10 Counties for {offense} Offenses')
+county_df = df[['County', offense]].dropna().sort_values(offense, ascending=False).head(10)
 st.table(county_df)
