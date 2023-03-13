ef71747f85aafc906db9423da7084fcf342e8e00
Helmkampmc <120995077+Helmkampmc@users.noreply.github.com>
2023-03-13 15:07:20 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -2,8 +2,7 @@ import pandas as pd
 import streamlit as st
 
 # Read in the Michigan crime data
-url = 'https://www.michigan.gov/documents/msp/Crime_in_Michigan_2021_745549_7.xlsx'
-df = pd.read_excel(url, skiprows=2, sheet_name='Table 7', header=[0,1,2])
+df = pd.read_excel(Agency Crime Stats_2021.xlsx, skiprows=2, sheet_name='Table 7', header=[0,1,2])
 
 # Rename columns for easier access
 df.columns = [f'{col[0]}_{col[1].replace(" ", "_")}_{col[2].replace(" ", "_")}' for col in df.columns]
