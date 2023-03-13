fcc55e7e7b4fe93ac3aef9ecce735af8412e11c8
Helmkampmc <120995077+Helmkampmc@users.noreply.github.com>
2023-03-13 15:12:26 -0400

    Update crime_analysis.py

--- crime_analysis.py
+++ crime_analysis.py
@@ -1,20 +1,55 @@
 import pandas as pd
 import streamlit as st
+import altair as alt
 
-# Read in the Michigan crime data
-df = pd.read_excel("Agency Crime Stats_2021.xlsx", skiprows=2, sheet_name='Table 7', header=[0,1,2])
+# Read in the crime data
+url = 'https://www.michigan.gov/msp/-/media/Project/Websites/msp/micr-assets/2021/Agency-Crime-Stats_2021.xlsx'
+df = pd.read_excel(url)
 
 # Rename columns for easier access
-df.columns = [f'{col[0]}_{col[1].replace(" ", "_")}_{col[2].replace(" ", "_")}' for col in df.columns]
+df.columns = [col.replace(" ", "_") for col in df.columns]
 
 # Create a sidebar with filtering options
 st.sidebar.title('Filter Options')
-police_dept = st.sidebar.selectbox('Select Police Department', options=df['Agency_Name_County'].unique())
+filter_col = st.sidebar.selectbox('Select Column to Filter By', options=df.columns)
 
-# Filter the data by police department
-filtered_df = df[df['Agency_Name_County'] == police_dept]
+# Create a filter for the selected column
+filter_val = st.sidebar.text_input(f'Select Value for {filter_col}', '')
 
-# Create a table showing the top 5 crimes for the selected police department
-st.title(f'Top 5 Crimes for {police_dept}')
-crime_df = filtered_df[['Offense_Description', 'Total_Offenses']].dropna().sort_values('Total_Offenses', ascending=False).head(5)
-st.table(crime_df)
+# Filter the data by the selected column and value
+if filter_val != '':
+    filtered_df = df[df[filter_col].str.contains(filter_val, case=False)]
+else:
+    filtered_df = df
+
+# Create a title for the app
+st.title('Michigan Crime Data')
+
+# Create a bar chart showing the number of incidents by agency type
+st.title('Number of Incidents by Agency Type')
+chart1 = alt.Chart(filtered_df).mark_bar().encode(
+    x=alt.X('Agency_Type:N', sort=alt.EncodingSortField(field='Total_Incidents', order='descending')),
+    y=alt.Y('Total_Incidents:Q', title='Number of Incidents'),
+    tooltip=['Agency_Type', 'Total_Incidents']
+).properties(
+    width=700,
+    height=500
+)
+st.altair_chart(chart1)
+
+# Create a bar chart showing the number of arrests by agency type
+st.title('Number of Arrests by Agency Type')
+chart2 = alt.Chart(filtered_df).mark_bar().encode(
+    x=alt.X('Agency_Type:N', sort=alt.EncodingSortField(field='Total_Arrests', order='descending')),
+    y=alt.Y('Total_Arrests:Q', title='Number of Arrests'),
+    tooltip=['Agency_Type', 'Total_Arrests']
+).properties(
+    width=700,
+    height=500
+)
+st.altair_chart(chart2)
+
+# Create a table showing the total incidents and arrests for each agency type
+st.title('Total Incidents and Arrests by Agency Type')
+sum_df = filtered_df.groupby('Agency_Type')[['Total_Incidents', 'Total_Arrests']].sum()
+st.table(sum_df)
